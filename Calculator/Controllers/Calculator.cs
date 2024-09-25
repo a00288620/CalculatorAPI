3c2c7bb5761a49339b6ee9c14b7c986acf5bb051
a00288620 <a00288620@mycambrian.ca>
2024-09-24 21:25:53 -0400

    Add files via upload

---
+++ Calculator/Controllers/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Calculator : ControllerBase
+    {
+        [HttpGet("add")]
+        public ActionResult<double> Add(double a, double b)
+        {
+            return Ok(a + b);
+        }
+
+        [HttpGet("subtract")]
+        public ActionResult<double> Subtract(double a, double b)
+        {
+            return Ok(a - b);
+        }
+
+        [HttpGet("multiply")]
+        public ActionResult<double> Multiply(double a, double b)
+        {
+            return Ok(a * b);
+        }
+
+        [HttpGet("divide")]
+        public ActionResult<double> Divide(double a, double b)
+        {
+            if (b == 0)
+                return BadRequest("Cannot divide by zero.");
+
+            return Ok(a / b);
+        }
+    }
+}
+
